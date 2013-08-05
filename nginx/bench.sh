3c03a8e21191bed4009291f04f8e39862736fbb1
Erik Hollensbe <erik+github@hollensbe.org>
2013-08-04 17:07:46 -0700

    nginx tests -- still more to do

---
+++ nginx/bench.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+# vim: ft=sh sw=2 ts=2 st=2 sts=2 et
+#
+# NOTE
+#
+# see the bottom of this script for all the test batteries executed as
+# functions -- comment out what you don't want to run!
+#
+# edit the variables below to tweak some common parameters.
+
+# these are parameters to the `wrk` utility at
+# https://github.com/wg/wrk 
+# Please see the help for additional parameters / documentation
+open_connections=200
+time_to_run=60s
+# by default, set to number of cpus
+# just change this to a number if you want to use something fixed
+num_threads=$(cat /proc/cpuinfo | grep processor | wc -l)
+image_name=nginx
+
+header() {
+  echo
+  echo "---------"
+  echo $*
+  echo "---------"
+  echo
+}
+
+build_docker_image() {
+  header "Building Docker Image"
+  docker build -t $image_name .
+}
+
+install_nginx() {
+  header "Installing nginx locally if not already installed"
+  apt-get install -y nginx
+  header "Turning off nginx's service so we can run it ourselves"
+  service nginx stop
+}
+
+install_wrk() {
+  header "Installing wrk locally"
+  apt-get install -y build-essential git-core libssl-dev
+  if ! which wrk 2>&1 >/dev/null
+  then
+    git clone https://github.com/wg/wrk 
+    cd wrk 
+    make 
+    cp wrk /usr/local/bin
+    hash -r
+  fi
+}
+
+start_nginx() {
+  echo "killall nginx 2>&1 >/dev/null; nginx"
+}
+
+run_wrk() {
+  echo "wrk -c ${open_connections} -d ${time_to_run} -t ${num_threads} http://localhost:${1}"
+}
+
+run_nocache_test() { 
+  header "Running local test against port 80"
+  bash -c "$(start_nginx)"
+  bash -c "$(run_wrk 80)"
+
+  header "Docker Test 1: Locally in Docker"
+  docker run -t $image_name bash -c "$(start_nginx); $(run_wrk 80)"
+}
+
+build_docker_image
+install_nginx
+install_wrk
+run_nocache_test
