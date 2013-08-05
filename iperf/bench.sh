aeca1fc598d8d540da166ee4940d4fe77b979b0b
Erik Hollensbe <erik+github@hollensbe.org>
2013-08-04 22:11:13 -0700

    UDP testing.

--- iperf/bench.sh
+++ iperf/bench.sh
@@ -36,7 +36,7 @@ run_tcp_test() {
   header "TCP Test: localhost only"
   iperf -s -D
   iperf -f m -c localhost -P ${num_threads}
-  killall iperf
+  killall -KILL iperf
 
   header "TCP Test: through bridge"
   docker run -p 5001 $image_name bash -c "iperf -s" &
@@ -48,6 +48,23 @@ run_tcp_test() {
   docker rm ${docker_container}
 }
 
+run_udp_test() {
+  header "UDP Test: localhost only"
+  iperf -u -s -D
+  iperf -f m -u -c localhost -P ${num_threads}
+  killall -KILL iperf
+
+  header "UDP Test: through bridge"
+  docker run -p 5001 $image_name bash -c "iperf -u -s" &
+  sleep 1
+  docker_container=$(docker ps -q)
+  docker_port=$(docker port ${docker_container} 5001)
+  iperf -f m -u -c localhost -p ${docker_port} -P ${num_threads}
+  docker stop ${docker_container}
+  docker rm ${docker_container}
+}
+
 build_docker_image
 install_iperf
 run_tcp_test
+run_udp_test
